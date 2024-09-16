3906236cbe2380fc7688b8041273e243678a2a44
Felix Mauncho <39988041+Fellher@users.noreply.github.com>
2024-09-16 19:05:22 +0300

    onfigured docker compose and checks to wait for db
    onfigured docker compose and checks to wait for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
+
 ]
 
 MIDDLEWARE = [
@@ -79,8 +82,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
